036c534406545ac52e5a0e513b050852b00c4b15
nottebianca <sreckolazic94@gmail.com>
2021-08-19 12:42:13 +0200

    food order app

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
-import CartIcon from  '../Cart/CartIcon';
+import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+    const [btnBumpToggler, setBtnBumpToggler] = useState(false);
     const cartContext = useContext(CartContext);
+    const { items } = cartContext;
 
-    return <button className={classes.button} onClick={props.onClick}>
+    const itemsInCart = items.reduce((currentNum, item) => {
+        return currentNum + item.amount;
+    }, 0);
+
+    const btnClasses = `${classes.button} ${btnBumpToggler ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnBumpToggler(true);
+
+        const timer = setTimeout(() => {
+            setBtnBumpToggler(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
         <span>Your Cart</span>
         <span className={classes.badge}>
-            {cartContext.items.length}
+            {itemsInCart}
         </span>
     </button>
 }
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,29 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useRef } from 'react';
 import classes from './MealItemForm.module.css';
 import CartContext from '../../../store/cart-context';
 import Input from '../../UI/Input';
 
 const MealItemForm = (props) => {
+    const [isAmountValid, setIsAmountValid] = useState(true);
+    const mealAmount = useRef();
     const { meal } = props;
     const cartContext = useContext(CartContext);
 
     const onAddMeal = (e) => {
         e.preventDefault();
+        const enteredMealAmount = Number(mealAmount.current.value);
+        meal.amount = enteredMealAmount
         cartContext.addItem(meal);
-        console.log(cartContext);
-    } 
+
+        if (enteredMealAmount < 1 || enteredMealAmount > 5) {
+            setIsAmountValid(false);
+            return;
+        }
+    }
 
     return (
-        <form className={classes.form}>
-            <Input label="Amount" input={{
-                id: "amount_" + meal.id,
-                type: "number",
-                min: "1",
-                max: "5",
-                step: "1",
-                defaultValue:"1"
-            }} />
-            <button onClick={onAddMeal}>+ Add</button>
+        <form className={classes.form} onSubmit={onAddMeal}>
+            <Input
+                label="Amount"
+                ref={mealAmount}
+                input={{
+                    id: "amount_" + meal.id,
+                    type: "number",
+                    min: "1",
+                    max: "5",
+                    step: "1",
+                    defaultValue: "1",
+                }} />
+            <button>+ Add</button>
+            {!isAmountValid && <p>Please enter valid amount (1-5).</p>}
         </form>
     )
 }
